a410bf4ae70536068198b1d7de756750a6d95164
Aldi Andika <aldi.andika@gmail.com>
2022-06-10 09:52:45 +0700

    Turn in challenge 7

--- promise.js
+++ promise.js
@@ -1,7 +1,36 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emosi) => {
+  return new Promise((resolve, reject) => {
+    promiseTheaterIXX()
+      .then((data) => {
+        let count = 0;
+        for (let item = 0; item < data.length; item++) {
+          if (data[item].hasil === emosi) {
+            count += 1;
+          }
+        }
+
+        promiseTheaterVGC()
+          .then((data) => {
+            for (let item = 0; item < data.length; item++) {
+              if (data[item].hasil === emosi) {
+                count += 1;
+              }
+            }
+
+            // Resolve of promiseOutput
+            resolve(count);
+          })
+
+          // Reject of promiseOutput
+          .catch((err) => reject(err));
+      })
+      // Reject of promiseOutput
+      .catch((err) => reject(err));
+  });
+};
 
 module.exports = {
   promiseOutput,
